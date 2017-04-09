82c4bdf1b688c35a9c67659b5bf3485ec6fecaac
Ilari Juvani <ilari.juvani@metropolia.fi>
2017-04-09 19:49:07 +0300

    tested and made improvements to contact form

--- wp-content/themes/smoy/footer.php
+++ wp-content/themes/smoy/footer.php
@@ -11,51 +11,21 @@
 <?php wp_footer(); ?>
 
 <script type="text/javascript">
-    /*
-    jQuery(".person-content-wrapper img").hover(function(){
-        $(this).css("background-color", "yellow");
-        }, function(){
-        $(this).css("background-color", "pink");
-    });
-    */
+jQuery(function() {
+    jQuery('.wpcf7-form span.ajax-loader').replaceWith('<div class="ajax-loader">Loading...</div>');
+});
+
+function delayedReplace(){
+    jQuery('.wpcf7-form div.ajax-loader').replaceWith('<div class="ajax-loader">Loading...</div>');
+}
+
+var timerInit;
+window.onresize = function(){
+    clearTimeout(timerInit);
+    timerInit = setTimeout(delayedReplace, 600);
+}    
     
-    
-    
-    //var temp = "<div class='staff-box' style='width:{width}px; height: {height}px;'><div class='person-stretchy-wrapper'><div class='person-content-wrapper' style='background: {thisUrl}; background-position: center !important;'><div class='person-content'></div></div></div></div>";
-			
-    
-    
-    // 16 people
-    /*
-    var staffBgUrls = ['url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Maria_Blomberg.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/jarno_laitinen@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Jukka_Illikainen.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/esa_sonninen@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/teemu_eskola.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/jyri_huhtala@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Kimmo_Levonen@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/marini_jarvenpaa_graafihrj.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Mari_Eramaa@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Katja_Himanen.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Leyla_Avsar@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Pekka_Palonkorpi@2x.JPG")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Dani_Raatikainen_@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/rocky_rinne@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Tarja_Hietanen@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Anna_Vihra@2x.jpg")'];
-    
-    var sizeBooleans = [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0];
-    */
-    
-    
-    
-    // 15 people
-    
-    /*
-    
-    var staffBgUrls = ['url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Maria_Blomberg.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Kimmo_Levonen@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Katja_Himanen.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/teemu_eskola.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/jarno_laitinen@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Jukka_Illikainen.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/esa_sonninen@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/jyri_huhtala@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/marini_jarvenpaa_graafihrj.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Mari_Eramaa@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Leyla_Avsar@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Pekka_Palonkorpi@2x.JPG")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Dani_Raatikainen_@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/rocky_rinne@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Tarja_Hietanen@2x.jpg")'];
-    
-    var sizeBooleans = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    
-    */
-    
-    // 14 people
-    /*
-    var staffBgUrls = ['url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Maria_Blomberg.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/jarno_laitinen@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Jukka_Illikainen.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/esa_sonninen@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/teemu_eskola.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/jyri_huhtala@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Kimmo_Levonen@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/marini_jarvenpaa_graafihrj.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Mari_Eramaa@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Katja_Himanen.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Leyla_Avsar@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Pekka_Palonkorpi@2x.JPG")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/Dani_Raatikainen_@2x.jpg")', 'url("http://192.168.11.6:8083/wp-content/themes/smoy/img/ihmiset/rocky_rinne@2x.jpg")'];
-    
-    var sizeBooleans = [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0];
-    */
-    
-    
-  
-    
-    
-		</script>
+</script>
 
 </body>
 </html>    
